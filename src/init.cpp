0556d457a8eb3e5aeb082e26a1600984a327ae71
sirbond <erfan007p@gmail.com>
2021-05-24 00:36:43 +0700

    Bump copyright years in configure.ac
    gen-manpages
    update copyright holders
    update splash.png
    s/dashd/gobyted/

--- src/init.cpp
+++ src/init.cpp
@@ -671,7 +671,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/gobytecoin/gobyte>";
     const std::string URL_WEBSITE = "<https://gobyte.network>";
 
-    return CopyrightHolders(_("Copyright (C)"), 2014, COPYRIGHT_YEAR) + "\n" +
+    return CopyrightHolders(_("Copyright (C)"), 2017, COPYRIGHT_YEAR) + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
