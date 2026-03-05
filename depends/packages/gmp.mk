cc8f5b957cf046fef6c9ee3db222c86ef501bed6
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-03-05 17:11:44 +0200

    build(depends): update mirrors

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,7 +1,8 @@
 package=gmp
 $(package)_version=6.1.2
 #$(package)_download_path=https://gmplib.org/download/gmp
-$(package)_download_path=https://mirrors.sjtug.sjtu.edu.cn/gnu/gmp
+#$(package)_download_path=https://mirrors.sjtug.sjtu.edu.cn/gnu/gmp --- FALLBACK ---
+$(package)_download_path=https://mirrors.mit.edu/macports/distfiles/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
 $(package)_sha256_hash=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
 
