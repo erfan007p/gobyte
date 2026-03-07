a69829b289ad6d47f560c38b0486895bf8034ec4
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-03-07 03:52:14 +0200

    build(depends): update SHA256 hash

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -1,8 +1,8 @@
 package=qrencode
 $(package)_version=3.4.4
-$(package)_download_path=https://github.com/fukuchi/libqrencode/archive/
-$(package)_file_name=v$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=efe5188b1ddbcbf98763b819b146be6a90481aac30cfc8d858ab78a19cde1fa5
+$(package)_download_path=https://github.com/fukuchi/libqrencode/archive/refs/tags
+$(package)_file_name=v$($(package)_version).tar.gz
+$(package)_sha256_hash=ab7cdf84e3707573a39e116ebd33faa513b306201941df3f5e691568368d87bf
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared -without-tools --disable-sdltest
